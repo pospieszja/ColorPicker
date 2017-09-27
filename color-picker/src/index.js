9c562501290d7e6d7f35d7250dc643af2faf321a
pospieszja <j.pospieszynski@outlook.com>
2017-09-27 22:18:45 +0200

    Added convertion from HEX to RGB

--- color-picker/src/index.js
+++ color-picker/src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const REGEX_RGB = /^rgb\((\d{1,3}),(\d{1,3}),(\d{1,3})\)$/;
+const REGEX_HEX = /^#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})$/;
+//const REGEX_HSL = /^rgb\((\d{1,3}),(\d{1,3}),(\d{1,3})\)$/;
+
+function convertToRgb(val) {
+    var colors;
+    if(val.match(REGEX_RGB))
+    {
+        return val;
+    }
+    
+    if(val.match(REGEX_HEX))
+    {
+        colors = val.match(REGEX_HEX);
+        colors.splice(0,1)
+        return "rgb(" + parseInt(colors[0],16) + "," + parseInt(colors[1],16) + "," + parseInt(colors[2],16) +")";
+    }  
+}
+
 class ColorPicker extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             color: ""
-        }
-        
+        }        
     }
+
 
     handleInput(evt) {
         this.setState({
@@ -19,15 +38,17 @@ class ColorPicker extends React.Component {
         const styleColor = {
             backgroundColor: this.state.color
         }
+        
+        const rgb = convertToRgb(this.state.color);
 
         return (
             <div className="color-picker">
                 <input type="text" value={this.state.color} onChange={this.handleInput.bind(this)}/>
                 <div className="picked-color" style={styleColor}></div>
+                <p>{rgb}</p>
             </div>
         );
     }
-
 }
 
 ReactDOM.render(<ColorPicker />, document.getElementById('app'));
