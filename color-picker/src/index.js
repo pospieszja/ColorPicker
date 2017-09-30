f9e307717f65cc9c4bfb755585e2384f88d4277c
pospieszja <j.pospieszynski@outlook.com>
2017-09-30 13:48:42 +0200

    Change return value to "temlate string"

--- color-picker/src/index.js
+++ color-picker/src/index.js
@@ -16,7 +16,7 @@ function convertToRgb(val) {
     {
         colors = val.match(REGEX_HEX);
         colors.splice(0,1)
-        return "rgb(" + parseInt(colors[0],16) + "," + parseInt(colors[1],16) + "," + parseInt(colors[2],16) +")";
+        return `rgb(${parseInt(colors[0],16)}, ${parseInt(colors[1],16)}, ${parseInt(colors[2],16)})`;
     }  
 }
 
@@ -31,7 +31,7 @@ function convertToHex(val) {
     {
         colors = val.match(REGEX_RGB);
         colors.splice(0,1)
-        return "#" + colors[0].toString(16) + colors[1].toString(16) + colors[2].toString(16);
+        return `#${colors[0].toString(16)}${colors[1].toString(16)}${colors[2].toString(16)}`;
     }  
 }
 
