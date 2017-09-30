65d718f11748ec35d6ff750cd7b0ee3c8bdf7f6b
pospieszja <j.pospieszynski@outlook.com>
2017-09-30 15:08:04 +0200

    Added convertion from HSL to RGB format

--- color-picker/src/index.js
+++ color-picker/src/index.js
@@ -28,7 +28,7 @@ class ColorPicker extends React.Component {
     render() {
 
         const REGEX_RGB = /^rgb\((\d{1,3}),(\d{1,3}),(\d{1,3})\)$/;
-        const REGEX_HEX = /^#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})$/;   
+        const REGEX_HEX = /^#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})$/;
         const REGEX_HSL = /^hsl\((\d{1,3})\,(\d{1,3})\%\,(\d{1,3})\%\)$/;
 
         const inputColorFormat = (color) => {
@@ -37,7 +37,7 @@ class ColorPicker extends React.Component {
             if (REGEX_HEX.test(color))
                 return "hex";
             if (REGEX_HSL.test(color))
-                return "hsl";                
+                return "hsl";
         };
 
         const convertToRgb = (format, color) => {
@@ -50,6 +50,62 @@ class ColorPicker extends React.Component {
                 rgb = color.match(REGEX_HEX);
                 rgb.splice(0, 1)
                 return `rgb(${parseInt(rgb[0], 16)}, ${parseInt(rgb[1], 16)}, ${parseInt(rgb[2], 16)})`;
+            }
+
+            if (format === "hsl") {
+                let hsl = [];
+                let h, s, l;
+                let c, x, m;
+                let r, g, b;
+
+                hsl = color.match(REGEX_HSL)
+                hsl.splice(0, 1)
+
+                h = hsl[0];
+                s = hsl[1] / 100;
+                l = hsl[2] / 100;
+
+                c = (1 - Math.abs(2 * l - 1)) * s;
+                x = c * (1 - Math.abs((h / 60) % 2 - 1))
+                m = l - c / 2
+
+                if (h >= 0 && h < 60) {
+                    r = c;
+                    b = x;
+                    g = 0;
+                }
+
+                if (h >= 60 && h < 120) {
+                    r = x;
+                    b = c;
+                    g = 0;
+                }
+
+                if (h >= 120 && h < 180) {
+                    r = 0;
+                    b = c;
+                    g = x;
+                }
+
+                if (h >= 180 && h < 240) {
+                    r = 0;
+                    b = x;
+                    g = c;
+                }
+
+                if (h >= 240 && h < 300) {
+                    r = x;
+                    b = 0;
+                    g = c;
+                }
+
+                if (h >= 300 && h < 360) {
+                    r = c;
+                    b = 0;
+                    g = x;
+                }
+
+                return `rgb(${Math.round((r + m) * 255)}, ${Math.round((b + m) * 255)}, ${Math.round((g + m) * 255)})`
             }
         }
 
